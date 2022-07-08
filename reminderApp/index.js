8657a9f129a3084099b465a30924b3ebda93d5bb
rosalia-matzumiya <rmg2694@gmail.com>
2022-07-07 17:31:54 -0700

    added passport and, middleware, seesion info

--- reminderApp/index.js
+++ reminderApp/index.js
@@ -1,18 +1,49 @@
 const express = require("express");
-const app = express();
-const path = require("path");
 const ejsLayouts = require("express-ejs-layouts");
-const reminderController = require("./controller/reminder_controller");
-const authController = require("./controller/auth_controller");
+const session = require("express-session");
+const reminderController = require("./controllers/reminder_controller");
+const authController = require("./controllers/auth_controller");
+const path = require("path");
 const port = process.env.port || 3001;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(express.urlencoded({ extended: false }));
-
-app.use(ejsLayouts);
+const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+const passport = require("./middleware/passport");
+
+// Middleware for express
+app.use(express.json());
+app.use(ejsLayouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Session information
+app.use((req, res, next) => {
+  console.log(`User details are: `);
+  console.log(req.user);
+
+  console.log("Entire session object:");
+  console.log(req.session);
+
+  console.log(`Session details are: `);
+  console.log(req.session.passport);
+  next();
+});
 
 // Routes start here
 
